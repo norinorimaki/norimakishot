e69c0891d18184865a1e79336e874c93f409bf96
norimaki <mamimume_fujimaki@outlook.jp>
2021-08-06 17:43:57 +0900

    名前変更＋ゲームシーン遷移

---
+++ 2d_norimakishot/Assets/Start_Button.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Start_Button : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("Game_Scene");
+    }
+}
